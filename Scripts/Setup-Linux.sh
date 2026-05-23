8e546da16bb517b7bc4288381767b2401e4af1c3
Ohm Desai <odesai840@gmail.com>
2026-05-23 16:07:22 -0400

    changed default build type on windows to release

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -5,3 +5,4 @@ git submodule update --init --recursive
 cmake -S . -B Build -DCMAKE_BUILD_TYPE=Release
 cmake --build Build
 popd
+read -p "Press Enter to continue..."
--- Scripts/Setup-Windows.bat
+++ Scripts/Setup-Windows.bat
@@ -2,7 +2,7 @@
 
 pushd ..
 git submodule update --init --recursive
-cmake -S . -B Build
+cmake -S . -B Build -DCMAKE_BUILD_TYPE=Release
 cmake --build Build
 popd
 pause
